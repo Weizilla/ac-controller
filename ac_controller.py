192d40496141db05b76e3a6044ed217ed3620c9f
Wei Yang <wei@weizilla.com>
2025-09-28 13:49:54 -0700

    more loops

--- ac_controller.py
+++ ac_controller.py
@@ -8,7 +8,8 @@ from device import Device
 logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
-SLEEP_INTERVAL = 10 * 60 * 1000
+LOOP_INTERVAL = 60 * 1000
+CHECK_INTERVAL = 10 * 60 * 1000
 HIGH_TEMP_THRESHOLD = 80
 LOW_TEMP_THRESHOLD = 75
 
@@ -21,7 +22,7 @@ class ACController(object):
     def __init__(self, device: Device):
         self.device = device
         self.last_check = None
-        self.next_run = None
+        self.next_check = None
 
     def get_action(self) -> Action:
         value = self.device.value()
@@ -40,17 +41,18 @@ class ACController(object):
     def run_loop(self):
         while True:
             self.run_once()
-            time.sleep(SLEEP_INTERVAL)
+            time.sleep(LOOP_INTERVAL)
 
     def run_once(self):
-        action = self.get_action()
-        self.last_check = datetime.now()
-        self.next_run = datetime.now() + timedelta(milliseconds=SLEEP_INTERVAL)
+        if self.next_check is None or datetime.now() >= self.next_check:
+            action = self.get_action()
+            self.last_check = datetime.now()
+            self.next_check = datetime.now() + timedelta(milliseconds=CHECK_INTERVAL)
 
-        if action == Action.TURN_ON:
-            self._turn_on()
-        elif action == Action.TURN_OFF:
-            self._turn_off()
+            if action == Action.TURN_ON:
+                self._turn_on()
+            elif action == Action.TURN_OFF:
+                self._turn_off()
 
         self._update_message()
 
@@ -64,10 +66,12 @@ class ACController(object):
         status = "ON " if self.device.value() else "OFF"
         temp = self.device.temperature()
         time = self.last_check.strftime("%I:%M:%S %p") if self.last_check else "NO TIME"
-        next_run_mins = int((self.next_run - datetime.now()).total_seconds() / 60)
+        next_check_total_seconds = (self.next_check - datetime.now()).total_seconds()
+        next_check_mins = int(next_check_total_seconds / 60)
+        next_check_seconds = int(next_check_total_seconds % 60)
 
         line_1 = f"{status}           {temp}"
-        line_2 = f"{time}   {next_run_mins}"
+        line_2 = f"{time} {next_check_mins}:{next_check_seconds}"
 
         self.device.set_lcd_message(line_1, line_2)
 
--- ac_controller_test.py
+++ ac_controller_test.py
@@ -1,15 +1,16 @@
 import logging
 import unittest
+from datetime import datetime, timedelta
+from freezegun import freeze_time
 from unittest.mock import Mock
 
-from ac_controller import ACController, Action
+from ac_controller import ACController, Action, CHECK_INTERVAL
 
 logging.disable()
 
 
 class MyTestCase(unittest.TestCase):
     device = Mock()
-    controller = ACController(device)
 
     param_list = [
         # current value, temperature, expected action
@@ -21,14 +22,49 @@ class MyTestCase(unittest.TestCase):
         (0, 90, Action.TURN_ON),
     ]
 
-    def test_action(self):
+    def setUp(self):
+        self.controller = ACController(self.device)
 
+    def test_action(self):
         for value, temp, expected in self.param_list:
             self.device.value = Mock(return_value=value)
             self.device.temperature = Mock(return_value=temp)
 
             actual = self.controller.get_action()
             self.assertEqual(actual, expected, f"value: {value}, temp: {temp}, actual: {actual}, expected: {expected}")
+
+    def test_run_once_action(self):
+        self.controller.get_action = Mock(return_value=Action.NO_OP)
+        self.controller.run_once()
+
+        self.controller.get_action.assert_called_once()
+
+    def test_run_once_no_repeated_action(self):
+        self.controller.get_action = Mock(return_value=Action.NO_OP)
+        self.controller.run_once()
+        self.controller.run_once()
+        self.controller.run_once()
+        self.controller.run_once()
+
+        self.controller.get_action.assert_called_once()
+
+    def test_run_once_action_after_check_interval(self):
+        now = datetime.now()
+
+        with freeze_time(now) as frozen_time:
+            self.controller.get_action = Mock(return_value=Action.NO_OP)
+            self.controller.run_once()
+
+            self.controller.get_action.assert_called_once()
+
+            new_now = now + timedelta(milliseconds=CHECK_INTERVAL + 1000)
+            frozen_time.move_to(new_now)
+
+            self.controller.run_once()
+
+            call_count = self.controller.get_action.call_count
+            self.assertEqual(call_count, 2)
+
 
 
 if __name__ == '__main__':
